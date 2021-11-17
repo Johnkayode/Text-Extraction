68b30d0d6bc036d6354faf9e323300070abcae59
nerdthejohn <newtonjohn043@gmail.com>
2021-11-17 08:43:57 +0100

    set up heroku

--- text_extraction/settings.py
+++ text_extraction/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,3 +117,4 @@ STATICFILES_DIRS = [BASE_DIR / "static"]
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
